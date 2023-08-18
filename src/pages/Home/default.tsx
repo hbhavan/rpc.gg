8505e65a6dc09b747a5f6ec5f0115ea0bd03fae1
hbhavan <hbhavan@gmail.com>
2023-08-18 10:39:38 -0400

    First Release 1.4

---
+++ src/pages/Home/default.tsx
@@ -0,0 +1,31 @@
+import { useNavigate } from 'react-router-dom'
+import { NavLink } from "../../components/NavMenu/styles"
+import { HomeContainer, SubTitle, Title, TitleContainer } from "./styles"
+
+
+
+export const DefaultPage = () => {
+    const navigate = useNavigate()
+   
+    return (
+        <HomeContainer
+            flexDir="column"
+            justifyContent="center"
+            alignItems="center"
+        >
+                <Title>
+                    Work In Progress
+                </Title>
+                <SubTitle>
+                    For the RPC Friend Point rankings, please go here
+                </SubTitle>
+                <NavLink
+                    onClick={() => {
+                        navigate('/rpc')
+                    }}
+                    color="zara.300"
+                >
+                    {'[gearetical.com/rpc]'}
+                </NavLink>
+        </HomeContainer>
+)}
